529d573175d8680eb61098ccecbc4cdb8797371a
hossein mint <info@mndco.ir>
2020-10-18 01:21:09 +0330

    remove davincho/tabulapdf-php-wrapper from required packages

--- src/IranianHoliday.php
+++ src/IranianHoliday.php
@@ -3,7 +3,7 @@
 namespace MNDCo\IranianHoliday;
 
 use Carbon\Carbon;
-use Davincho\Tabula\Tabula;
+//use Davincho\Tabula\Tabula;
 use GeniusTS\HijriDate\Hijri;
 use Morilog\Jalali\Jalalian;
 
@@ -75,51 +75,52 @@ class IranianHoliday
         $holy_days = json_decode(file_get_contents(__DIR__ . $this->holidays_json_file), true);
 
         if (!@$holy_days[$jalalian_date_time->getYear()] && $jalalian_date_time->getYear() <= jdate(Carbon::now())->getYear() + 1 && $jalalian_date_time->getYear() >= 1369) {
-            $full_array = [];
-
-            $file_name = "Data%5Choliday-" . $jalalian_date_time->getYear() . ".pdf";
-            if ($c = file_get_contents("https://calendar.ut.ac.ir/Fa/Tyear/" . $file_name)) {
-                file_put_contents($file_name, $c);
-            }
-
-            if (!file_exists($file_name)) {
-                return @$this->hijri_holidays[$arabic_date_time->month . "/" . $arabic_date_time->day];
-            }
-
-            $pdf = new Tabula($file_name);
-
-            $content = $pdf->parse();
-            unlink($file_name);
-
-
-            foreach (explode("\n", $content) as $key => $row) {
-                if ($key != 0) {
-                    $cols = explode(',', $row);
-                    if (@$cols[1] && $cols[2]) {
-
-                        $date = $this->fix_jalali_months($cols[2]);
-                        $date = str_replace(" ", "-", $jalalian_date_time->getYear() . " " . $date);
-                        $jdate_array = explode('-', $date);
-
-                        foreach ($jdate_array as $key => $ss)
-                            if ($key !== 0)
-                                $jdate_array[$key] = str_pad($jdate_array[$key], 2, 0, STR_PAD_LEFT);
-
-
-                        $jdate = Jalalian::fromFormat('Y-m-d', implode($jdate_array, '-'));
-                        $hdate_array = $this->fix_hijri_months($cols[1]);
-
-
-                        $hdate = $hdate_array[1] . "/" . $hdate_array[2];
-                        $full_array[$jdate->getYear()][$jdate->getDayOfYear()] = @$this->hijri_holidays[$hdate];
-                    }
-                }
-
-            }
-            foreach ($full_array as $key => $data)
-                $holy_days[$key] = $data;
-
-            file_put_contents(__DIR__ . $this->holidays_json_file, json_encode($holy_days));
+            // need java for update json file
+//            $full_array = [];
+//
+//            $file_name = "Data%5Choliday-" . $jalalian_date_time->getYear() . ".pdf";
+//            if ($c = file_get_contents("https://calendar.ut.ac.ir/Fa/Tyear/" . $file_name)) {
+//                file_put_contents($file_name, $c);
+//            }
+//
+//            if (!file_exists($file_name)) {
+//                return @$this->hijri_holidays[$arabic_date_time->month . "/" . $arabic_date_time->day];
+//            }
+//
+//            $pdf = new Tabula($file_name);
+//
+//            $content = $pdf->parse();
+//            unlink($file_name);
+//
+//
+//            foreach (explode("\n", $content) as $key => $row) {
+//                if ($key != 0) {
+//                    $cols = explode(',', $row);
+//                    if (@$cols[1] && $cols[2]) {
+//
+//                        $date = $this->fix_jalali_months($cols[2]);
+//                        $date = str_replace(" ", "-", $jalalian_date_time->getYear() . " " . $date);
+//                        $jdate_array = explode('-', $date);
+//
+//                        foreach ($jdate_array as $key => $ss)
+//                            if ($key !== 0)
+//                                $jdate_array[$key] = str_pad($jdate_array[$key], 2, 0, STR_PAD_LEFT);
+//
+//
+//                        $jdate = Jalalian::fromFormat('Y-m-d', implode($jdate_array, '-'));
+//                        $hdate_array = $this->fix_hijri_months($cols[1]);
+//
+//
+//                        $hdate = $hdate_array[1] . "/" . $hdate_array[2];
+//                        $full_array[$jdate->getYear()][$jdate->getDayOfYear()] = @$this->hijri_holidays[$hdate];
+//                    }
+//                }
+//
+//            }
+//            foreach ($full_array as $key => $data)
+//                $holy_days[$key] = $data;
+//
+//            file_put_contents(__DIR__ . $this->holidays_json_file, json_encode($holy_days));
         }
 
 
@@ -136,52 +137,53 @@ class IranianHoliday
     }
 
 
-    protected function fix_hijri_months($string)
-    {
-        $ro = preg_replace('/\s+/', ' ', $string);
-
-        $ro = str_replace("مﺮﺤﻣ", "1", $ro);
-        $ro = str_replace("ﺮﻔﺻ", "2", $ro);
-        $ro = str_replace("لوﻻا‌ ﻊﻴﺑر", "3", $ro);
-        $ro = str_replace("لوﻻا ﻊﻴﺑر", "3", $ro);
-        $ro = str_replace("لوﻻاﻊ ﻴﺑر", "3", $ro);
-        $ro = str_replace("لوﻻا ﻊﯿﺑر", "3", $ro);
-        $ro = str_replace("1422ﻲﻧﺎﺜﻟا يدﺎﻤﺟ", "1422 6", $ro);
-        $ro = str_replace("ﻪﻴﻧﺎﺜﻟا يدﺎﻤﺟ", "6", $ro);
-        $ro = str_replace("ﺔﯿﻧﺎﺜﻟاي دﺎﻤﺟ", "6", $ro);
-        $ro = str_replace("ﺔﻴﻧﺎﺜﻟاي دﺎﻤﺟ", "6", $ro);
-        $ro = str_replace("ﺔﻴﻧﺎﺜﻟا يدﺎﻤﺟ", "6", $ro);
-        $ro = str_replace("ﻲﻧﺎﺜﻟا يدﺎﻤﺟ", "6", $ro);
-        $ro = str_replace("ﻲﻧﺎﺜﻟاي دﺎﻤﺟ", "6", $ro);
-        $ro = str_replace("ﺐﺟر", "7", $ro);
-        $ro = str_replace("نﺎﺒﻌﺷ", "8", $ro);
-        $ro = str_replace("نﺎﻀﻣر", "9", $ro);
-        $ro = str_replace("لاﻮﺷ", "10", $ro);
-        $ro = str_replace("هﺪﻌﻘﻟا يذ", "11", $ro);
-        $ro = str_replace("هﺪﻌﻘﻳذ", "11", $ro);
-        $ro = str_replace("ﻪﺠﺤﻟا يذ", "12", $ro);
-        $ro = str_replace("ﺮﺧآ", "30", $ro);
-        $date = str_replace(" ", "-", $ro);
-        return explode('-', $date);
-    }
-
-    protected function fix_jalali_months($string)
-    {
-        $ro = preg_replace('/\s+/', ' ', $string);
-        $ro = str_replace("ﻦﻳدروﺮﻓ", "1", $ro);
-        $ro = str_replace("ﺖﺸﻬﺒﯾدرا", "2", $ro);
-        $ro = str_replace("ﺖﺸﻬﺒﻳدرا", "2", $ro);
-        $ro = str_replace("دادﺮﺧ", "3", $ro);
-        $ro = str_replace("ﺮﻴﺗ", "4", $ro);
-        $ro = str_replace("ﺮﯿﺗ", "4", $ro);
-        $ro = str_replace("دادﺮﻣ", "5", $ro);
-        $ro = str_replace("رﻮﻳﺮﻬﺷ", "6", $ro);
-        $ro = str_replace("رﻮﯾﺮﻬﺷ", "6", $ro);
-        $ro = str_replace("ﺮﻬﻣ", "7", $ro);
-        $ro = str_replace("نﺎﺑآ", "8", $ro);
-        $ro = str_replace("رذآ", "9", $ro);
-        $ro = str_replace("يد", "10", $ro);
-        $ro = str_replace("ﻦﻤﻬﺑ", "11", $ro);
-        return str_replace("ﺪﻨﻔﺳا", "12", $ro);
-    }
+    // need java for update json file
+//    protected function fix_hijri_months($string)
+//    {
+//        $ro = preg_replace('/\s+/', ' ', $string);
+//
+//        $ro = str_replace("مﺮﺤﻣ", "1", $ro);
+//        $ro = str_replace("ﺮﻔﺻ", "2", $ro);
+//        $ro = str_replace("لوﻻا‌ ﻊﻴﺑر", "3", $ro);
+//        $ro = str_replace("لوﻻا ﻊﻴﺑر", "3", $ro);
+//        $ro = str_replace("لوﻻاﻊ ﻴﺑر", "3", $ro);
+//        $ro = str_replace("لوﻻا ﻊﯿﺑر", "3", $ro);
+//        $ro = str_replace("1422ﻲﻧﺎﺜﻟا يدﺎﻤﺟ", "1422 6", $ro);
+//        $ro = str_replace("ﻪﻴﻧﺎﺜﻟا يدﺎﻤﺟ", "6", $ro);
+//        $ro = str_replace("ﺔﯿﻧﺎﺜﻟاي دﺎﻤﺟ", "6", $ro);
+//        $ro = str_replace("ﺔﻴﻧﺎﺜﻟاي دﺎﻤﺟ", "6", $ro);
+//        $ro = str_replace("ﺔﻴﻧﺎﺜﻟا يدﺎﻤﺟ", "6", $ro);
+//        $ro = str_replace("ﻲﻧﺎﺜﻟا يدﺎﻤﺟ", "6", $ro);
+//        $ro = str_replace("ﻲﻧﺎﺜﻟاي دﺎﻤﺟ", "6", $ro);
+//        $ro = str_replace("ﺐﺟر", "7", $ro);
+//        $ro = str_replace("نﺎﺒﻌﺷ", "8", $ro);
+//        $ro = str_replace("نﺎﻀﻣر", "9", $ro);
+//        $ro = str_replace("لاﻮﺷ", "10", $ro);
+//        $ro = str_replace("هﺪﻌﻘﻟا يذ", "11", $ro);
+//        $ro = str_replace("هﺪﻌﻘﻳذ", "11", $ro);
+//        $ro = str_replace("ﻪﺠﺤﻟا يذ", "12", $ro);
+//        $ro = str_replace("ﺮﺧآ", "30", $ro);
+//        $date = str_replace(" ", "-", $ro);
+//        return explode('-', $date);
+//    }
+//
+//    protected function fix_jalali_months($string)
+//    {
+//        $ro = preg_replace('/\s+/', ' ', $string);
+//        $ro = str_replace("ﻦﻳدروﺮﻓ", "1", $ro);
+//        $ro = str_replace("ﺖﺸﻬﺒﯾدرا", "2", $ro);
+//        $ro = str_replace("ﺖﺸﻬﺒﻳدرا", "2", $ro);
+//        $ro = str_replace("دادﺮﺧ", "3", $ro);
+//        $ro = str_replace("ﺮﻴﺗ", "4", $ro);
+//        $ro = str_replace("ﺮﯿﺗ", "4", $ro);
+//        $ro = str_replace("دادﺮﻣ", "5", $ro);
+//        $ro = str_replace("رﻮﻳﺮﻬﺷ", "6", $ro);
+//        $ro = str_replace("رﻮﯾﺮﻬﺷ", "6", $ro);
+//        $ro = str_replace("ﺮﻬﻣ", "7", $ro);
+//        $ro = str_replace("نﺎﺑآ", "8", $ro);
+//        $ro = str_replace("رذآ", "9", $ro);
+//        $ro = str_replace("يد", "10", $ro);
+//        $ro = str_replace("ﻦﻤﻬﺑ", "11", $ro);
+//        return str_replace("ﺪﻨﻔﺳا", "12", $ro);
+//    }
 }
